d28da3ed4b424900dc9ad5cfd413969e22ea4288
高鹏飞 <22373074@buaa.edu.cn>
2024-07-20 17:57:48 +0800

    由token获得用户id

--- UserInfo/views.py
+++ UserInfo/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from UserInfo.models import UserInfo
+from django.core.cache import cache
 # Create your views here.
+
+
+def getUserId(request):
+    token = request.GET.get('token')
+    if cache.get(token) is not None:
+        return cache.get(token)
+    else:
+        for it in UserInfo.objects.all():
+            if token == it.token:
+                cache.set(token, it.id, timeout=900)
+                return it.id
